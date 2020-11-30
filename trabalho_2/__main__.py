51bcb5b9438860cfb529a339035bec21705b1f59
Karina Mayumi Johansson <3rd.karina.johansson@b2wdigital.com>
2020-11-30 20:20:21 -0300

    add config file integration

--- trabalho_2/__main__.py
+++ trabalho_2/__main__.py
@@ -1,4 +1,25 @@
 from folha_crawler import FolhaCrawler
+import yaml
 
-cr = FolhaCrawler()
-news = cr.get_data()
+
+def get_config():
+    with open('config.yaml', 'r') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+        return config['crawler'], config['processing']
+
+
+def main():
+    crawlers, processings = get_config()
+
+    if crawlers is not None:
+        for crawler in crawlers:
+            c = eval(crawler)()
+            news = c.get_data()
+
+            if processings is not None:
+                for processing in processings:
+                    eval(processing)()
+
+
+if __name__ == "__main__":
+    main()
