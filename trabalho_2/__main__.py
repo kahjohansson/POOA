026e47d0ff3a247aefcac8e870d2430c903ae331
donderileo <leonardodonderi@estudante.ufscar.br>
2020-12-02 01:02:04 -0300

    write csv

--- trabalho_2/__main__.py
+++ trabalho_2/__main__.py
@@ -1,5 +1,10 @@
+#encoding: utf-8
+
 from folha_crawler import FolhaCrawler
+from uol_crawler import UolCrawler
 import yaml
+import csv
+
 
 
 def get_config():
@@ -19,7 +24,17 @@ def main():
             if processings is not None:
                 for processing in processings:
                     eval(processing)()
-
+    
+    #Escrita csv utf8 pra ler no vscode, latin1 pro excell.
+    
+    with open('teste.csv', 'w', encoding='utf8', newline='') as file:
+    
+        writer = csv.writer(file)
+        
+        writer.writerow(["titulo", "link"])
+        for tupla in news:
+           writer.writerow([tupla[0],tupla[1]])
+            
 
 if __name__ == "__main__":
     main()
